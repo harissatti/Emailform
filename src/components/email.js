bad7430980dd3127f4c31be96ec01227feb3fba9
harissatti <harisjaved669@gmail.com>
2021-12-08 13:05:25 +0500

    adding varities of sending mail(gmail,yahoo etx),add spinner on submit  button while data is fetching

--- src/components/email.js
+++ src/components/email.js
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from "react";
 
 export function Email() {
   const [Bulkemail, setBulkemail] = useState({});
-  const[data,setdata]=useState([]);
-  const[Verified,setverified]=useState([]);
-  const[unverified,setunverified]=useState([]);
+  const [data, setdata] = useState([]);
+  const[loading,setloading]=useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [getmail, setmail] = useState([]);
+  const [Value,setValue]=useState('');
   // geting form data
   let name;
   let value;
@@ -18,21 +18,24 @@ export function Email() {
     setBulkemail({ ...Bulkemail, [name]: value })
 
   }
+ 
 
-// postapi
+  // postapi
   const handelsubmit = async (e) => {
     e.preventDefault();
+    setloading(true);
     setIsLoading(true);
-      const res =await axios.post('https://bulkemails.herokuapp.com/api/checkmail',
-     { mail:Bulkemail.mail})
-     .then((data) =>{ setdata(data.data.data)})
-     console.log(data,"data") 
-     setIsLoading(false);
-   }
-  
+    const res = await axios.post(`https://bulkemails.herokuapp.com/api/checkmail/${Value}`,
+      { mail: Bulkemail.mail })
+      .then((data) => { setdata(data.data.data)
+      setloading(false) })
+    console.log(data, "data")
+    setIsLoading(false);
+  }
 
 
-// getapi
+
+  // getapi
   async function GetData() {
 
     let arr = [];
@@ -49,39 +52,61 @@ export function Email() {
   }, []);
 
   return (
-    <div className="container    col-6">
+    <div className="container-fluid ">
       <div className="row ">
         <div className="col">
           <form >
             <div className="mb-3">
-              <label htmlFor="email" className="form-label">Bulk Domains</label>
+              <label htmlFor="email" className="form-label fw-bold">Bulk Domains</label>
               <textarea type="text" name="mail" placeholder={Bulkemail.email}
                 onChange={handelinput} autoComplete="off" className="form-control" rows="5" id="email">
               </textarea>
             </div>
-            <button style={{ width: "100%" }} onClick={handelsubmit} className="btn btn-danger">Submit</button>
-          </form>
-          <div>  <label htmlFor="validemails" className="form-label mt-3 ">Verified</label></div>
-         
-         
-          {!isLoading ? data.verified.map((veri,i) => (
-            <div>
-             
-              <span className="d-block p-2 border text-black pt-2 " key={i}>{veri}</span></div>)): null}
+            <select className="form-select form-select-sm mb-3 " onChange={(e)=>{const select=e.target.value;
+            setValue(select);}} aria-label=".form-select-sm example">
+              <option  selected>select</option>
+              <option value="gmail.com">Gmail</option>
+              <option value="yahoo.com">Yahoo</option>
+              <option value="outlook.com">Outlook</option>
+              <option value="hotmail.com">Hotmail</option>
+            </select>
           
-              <div><label htmlFor="validemails" className="form-label mt-3">unverified</label></div>
-         
-         {!isLoading ? data.unverified.map((veri,i) => (
-           <div>
             
-             <span className="d-block p-2 border text-black pt-2 " key={i}>{veri}</span></div>)): null}
-          <label htmlFor="validemails" className="form-label mt-5">Valid emails</label>
+           {!loading && <button style={{ width: "100%" }} onClick={handelsubmit} className="btn btn-primary">Submit</button>}
+           {loading && <button style={{ width: "100%" }}  className="btn btn-primary" disabled>
+             <i className="fas fa-spinner fa-spin"></i>Submiting</button>}
+          </form>
 
-          {getmail.map((mail, i) => (
-            <div>
-              {console.log(getmail, "mailing")}
-              <span className="d-block p-2 border text-black pt-2 " key={i}>{mail}</span></div>))}
+
         </div>
+        <div className="col mt-0 pt-0 ">
+          <div>  <label htmlFor="validemails" className="form-label  fw-bold ">Verified</label></div>
+
+
+          {!isLoading ? data.verified.map((veri, i) => (
+            <div>
+
+              <span className="d-block p-2 border text-black pt-2 " key={i}>{veri}</span></div>)) : null}
+
+          <div><label htmlFor="validemails" className="form-label mt-4 fw-bold">unverified</label></div>
+
+          {!isLoading ? data.unverified.map((veri, i) => (
+            <div>
+
+              <span className="d-block p-2 border text-black pt-2 " key={i}>{veri}</span></div>)) : null}
+
+        </div>
+      </div>
+      <div className="row ">
+
+        <label htmlFor="validemails" className="form-label fw-bold mt-3 ">Valid emails</label>
+
+
+        {getmail.map((mail, i) => (
+          <div>
+            {console.log(getmail, "mailing")}
+            <span className="d-block p-2 border text-black pt-2 " key={i}>{mail}</span></div>))}
+
       </div>
     </div>)
 }
